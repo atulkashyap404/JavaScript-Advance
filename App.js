4ceabe820ae684bbdf3307a5a4978bc617dd25c3
atulkumarmanjhi <atulkashyapvlog@gmail.com>
2024-01-30 16:10:39 +0530

    add code

--- App.js
+++ App.js
@@ -1,2 +1,13 @@
 //******************** All DOM selector ****************************
 
+const head = document.querySelector('h2');
+console.log(head);
+
+const title = document.querySelector('#title');
+console.log(title);
+
+const heading = document.querySelector('.heading');
+console.log(heading);
+
+heading.style.backgroundColor = "green";
+
